f533bdf91564a1a935ae4feef161d92286581a37
balaji <balaji.s@quadkast.co.in>
2024-09-10 16:48:28 +0530

    check

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ app.use(bodyParser.urlencoded({
     extended: true,
     limit: "50mb"
 }))
-
+app.use('/', (req, res) => {
+    console.log(':::::::::')
+    res.send("balu")
+})
 app.use('/user', require('./Controller/User/Router'))
 app.use('/owner', require('./Controller/Owner/Router'))
 app.use('/movie', require('./Controller/Movies/Router'))
 app.use('/book', require('./Controller/Booking/Router'))
 app.use('/screen', require('./Controller/Screen/Router'))
-app.use('/rating',require('./Controller/Rating/Router'))
+app.use('/rating', require('./Controller/Rating/Router'))
 
 app.listen(PORT, () => {
     console.log("PORT RUNNING....")
